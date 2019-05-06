be4f5461f7308497f5c687c2516a2dbcf23ccd33
bencat07 <bencat07@gmail.com>
2019-05-06 20:04:09 +0200

    Add more needed stuff

--- includes/clip_lock_impl.h
+++ includes/clip_lock_impl.h
@@ -7,25 +7,30 @@
 #ifndef CLIP_LOCK_IMPL_H_INCLUDED
 #define CLIP_LOCK_IMPL_H_INCLUDED
 
-namespace clip {
+namespace clip
+{
 
-class lock::impl {
+class lock::impl
+{
 public:
-  impl(void* native_window_handle);
-  ~impl();
+    impl(void *native_window_handle);
+    ~impl();
 
-  bool locked() const { return m_locked; }
-  bool clear();
-  bool is_convertible(format f) const;
-  bool set_data(format f, const char* buf, size_t len);
-  bool get_data(format f, char* buf, size_t len) const;
-  size_t get_data_length(format f) const;
-  bool set_image(const image& image);
-  bool get_image(image& image) const;
-  bool get_image_spec(image_spec& spec) const;
+    bool locked() const
+    {
+        return m_locked;
+    }
+    bool clear();
+    bool is_convertible(format f) const;
+    bool set_data(format f, const char *buf, size_t len);
+    bool get_data(format f, char *buf, size_t len) const;
+    size_t get_data_length(format f) const;
+    bool set_image(const image &image);
+    bool get_image(image &image) const;
+    bool get_image_spec(image_spec &spec) const;
 
 private:
-  bool m_locked;
+    bool m_locked;
 };
 
 } // namespace clip
